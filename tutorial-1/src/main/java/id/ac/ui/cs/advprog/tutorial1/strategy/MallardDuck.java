c73a4dac3808597e055f3e21f2bdc022fb317104
farqodri <arfixfari@gmail.com>
2018-02-22 13:11:48 +0700

    FIxes

--- tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/MallardDuck.java
+++ tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/MallardDuck.java
@@ -2,15 +2,4 @@ package id.ac.ui.cs.advprog.tutorial1.strategy;
 
 public class MallardDuck extends Duck {
     // TODO Complete me!
-<<<<<<< HEAD
-=======
-	public MallardDuck() {
-		// TODO Auto-generated constructor stub
-		this.setQuackBehavior(new Quack());
-		this.setFlyBehavior(new FlyWithWings());
-	}
-	
-	public void display(){}
-	public void swim(){}
->>>>>>> branch_1
 }
--- tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/ModelDuck.java
+++ tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/ModelDuck.java
@@ -2,14 +2,4 @@ package id.ac.ui.cs.advprog.tutorial1.strategy;
 
 public class ModelDuck {
     // TODO Complete me!
-<<<<<<< HEAD
-=======
-	public ModelDuck() {
-		this.setQuackBehavior(new MuteQuack());
-		this.setFlyBehavior(new FlyNoWay());
-	}
-	
-	public void display(){}
-	public void swim(){}
->>>>>>> branch_1
 }
