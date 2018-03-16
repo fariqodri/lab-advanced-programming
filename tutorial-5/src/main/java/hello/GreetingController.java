6516a682e3548cfa4cd639ac5e58053b6811ec7d
farqodri <arfixfari@gmail.com>
2018-03-16 15:06:25 +0700

    Fix cv output format

--- tutorial-5/src/main/java/hello/GreetingController.java
+++ tutorial-5/src/main/java/hello/GreetingController.java
@@ -1,9 +1,11 @@
 package hello;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class GreetingController {
@@ -17,20 +19,20 @@ public class GreetingController {
         } else {
             model.addAttribute("title", name + ", I hope you interested to hire me");
         }
-        StringBuilder cv = new StringBuilder();
-        cv.append("Name: Fari Qodri Andana\n");
-        cv.append("Birthdate: 21 September 1999\n");
-        cv.append("Birthplace: Jakarta\n");
-        cv.append("Address: Kramat Jati\n");
-        cv.append("\t---Education---\n");
-        cv.append("Elementary School: SDI Muhammadiyah 2 Denpasar");
-        cv.append("Junior High School: SMPN 8 Malang");
-        cv.append("Senior High School: SMAN 8 Jakarta\n");
-        cv.append("University: University of Indonesia\n");
-        cv.append("---\tAbout Me---\n");
-        cv.append("I like to code and have fun\n");
-        cv.append("I am currently learning about Android\n");
-        model.addAttribute("info", cv.toString());
+        ArrayList<String> response = new ArrayList<>();
+        response.add("Name: Fari Qodri Andana\n");
+        response.add("Birthdate: 21 September 1999\n");
+        response.add("Birthplace: Jakarta\n");
+        response.add("Address: Kramat Jati\n");
+        response.add("\t---Education---\n");
+        response.add("Elementary School: SDI Muhammadiyah 2 Denpasar");
+        response.add("Junior High School: SMPN 8 Malang");
+        response.add("Senior High School: SMAN 8 Jakarta\n");
+        response.add("University: University of Indonesia\n");
+        response.add("---\tAbout Me---\n");
+        response.add("I like to code and have fun\n");
+        response.add("I am currently learning about Android\n");
+        model.addAttribute("info", response);
         return "greeting";
     }
 }
